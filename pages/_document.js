772869d25b747b2889d8d41369c2886cdedf175d
Factupos <factuposcolombia@gmail.com>
2025-04-17 14:35:40 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,14 +4,9 @@ export default function Document() {
   return (
     <Html lang="es">
       <Head>
-  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png" />
-  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png" />
-  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-  <link rel="manifest" href="/site.webmanifest" />
-  <meta name="theme-color" content="#ffffff" />
-  <title>FactuPOS</title>
-</Head>
-
+        <link rel="icon" href="/Factuposlogo.ico" />
+        <title>FactuPOS</title>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
