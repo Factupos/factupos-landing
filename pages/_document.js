b7ba5d576d4395f7ad3cf8821b1315182274d9c0
Factupos <factuposcolombia@gmail.com>
2025-04-17 13:57:40 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="es">
+      <Head>
+        <link rel="icon" href="/Factuposlogo.ico" />
+        <title>FactuPOS</title>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
