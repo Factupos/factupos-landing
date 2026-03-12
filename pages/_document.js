c88809867432bd77964552f7118b0107cbcfa1c8
Factupos <factuposcolombia@gmail.com>
2026-03-12 15:22:34 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="es">
       <Head>
+        <meta name="facebook-domain-verification" content="u8is3rp7ejoyvpk7ucrzj344qy2ykc" />
         <link rel="icon" href="/Factuposlogo.ico" />
         <title>FactuPOS</title>
       </Head>
